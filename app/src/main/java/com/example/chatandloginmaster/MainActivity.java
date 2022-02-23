401eb361a7ffbe16e53ee825ff5374915697e5c8
habilithm97 <habinkim97@gmail.com>
2022-02-23 23:40:00 +0900

    회원가입 기능 추가

--- app/src/main/java/com/example/chatandloginmaster/MainActivity.java
+++ app/src/main/java/com/example/chatandloginmaster/MainActivity.java
@@ -1,14 +1,81 @@
 package com.example.chatandloginmaster;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Hashtable;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private FirebaseAuth mAuth; // 파이어베이스 인증 객체
+
+    EditText idEdt, pwEdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        idEdt = (EditText)findViewById(R.id.idEdt);
+        pwEdt = (EditText)findViewById(R.id.pwEdt);
+
+        Button registerBtn = (Button)findViewById(R.id.registerBtn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = idEdt.getText().toString();
+                String pw = pwEdt.getText().toString();
+
+                if(id.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "아이디를 입력하세요. ", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(pw.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "패스워드를 입력하세요. ", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // 신규 회원가입
+                mAuth.createUserWithEmailAndPassword(id, pw).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()) { // 회원가입에 성공했다면
+                                    Log.d(TAG,  "회원 가입에 성공하였습니다. ");
+                                    Toast.makeText(MainActivity.this, "회원 가입에  성공하였습니다. ", Toast.LENGTH_SHORT).show();
+                                    FirebaseUser user = mAuth.getCurrentUser(); // 로그인한 회원의 계정정보 가져오기
+                                    //updateUI(user);
+                                } else {
+                                    Log.w(TAG, "회원 가입에 실패하였습니다. ", task.getException());
+                                    Toast.makeText(MainActivity.this, "회원 가입에 실패하였습니다. ", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // 앱이 시작되면 사용자가 로그인했는지 확인하고 그에 따라 UI를 업데이트함
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
     }
 }
