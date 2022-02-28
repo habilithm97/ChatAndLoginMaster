3b1005646fceca0c7be159647f54e77bb85d2818
habilithm97 <habinkim97@gmail.com>
2022-02-28 13:22:01 +0900

    RecyclerView 구현

--- app/src/main/java/com/example/chatandloginmaster/ChatActivity.java
+++ app/src/main/java/com/example/chatandloginmaster/ChatActivity.java
@@ -1,19 +1,52 @@
 package com.example.chatandloginmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ChatActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    RecyclerViewAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    EditText edt;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        EditText edt = (EditText)findViewById(R.id.edt);
-        Button btn = (Button)findViewById(R.id.btn);
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true); // 아이템을 추가해도 리사이클러뷰의 크기가 변하지 않음
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        String[] dataSet = {"테스트1", "테스트2", "테스트3", "테스트4"};
+        adapter = new RecyclerViewAdapter(dataSet);
+        recyclerView.setAdapter(adapter);
+
+        edt = (EditText)findViewById(R.id.edt);
+        btn = (Button)findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = edt.getText().toString();
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+
+                myRef.setValue("Hello, World!");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/chatandloginmaster/RecyclerViewAdapter.java
@@ -0,0 +1,45 @@
+package com.example.chatandloginmaster;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private String[] localDataSet;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
+
+        public ViewHolder(View view) {
+            super(view);
+            textView = (TextView) view.findViewById(R.id.tv);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+    public RecyclerViewAdapter(String[] dataSet) {
+        localDataSet = dataSet;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.text_item, viewGroup, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        viewHolder.getTextView().setText(localDataSet[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return localDataSet.length;
+    }
+}
